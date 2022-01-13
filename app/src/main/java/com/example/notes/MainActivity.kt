902b4fc68e82778db6d6601bf8aed6039b5bf836
Aritry Samaddar <aritrysamaddar00@gmail.com>
2022-01-13 15:34:03 +0530

    Notes Application

--- app/src/main/java/com/example/notes/MainActivity.kt
+++ app/src/main/java/com/example/notes/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.notes
 
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -10,11 +13,14 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), INotesRVAdapter {
 
-    lateinit var viewModel: NoteViewModel
+    private lateinit var viewModel: NoteViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        supportActionBar?.setBackgroundDrawable(ColorDrawable(resources.getColor(R.color.purple_200)))
 
         recyclerView.layoutManager = LinearLayoutManager(this)
         val adapter = NotesRVAdapter(this, this)
@@ -39,7 +45,14 @@ class MainActivity : AppCompatActivity(), INotesRVAdapter {
         val noteText = input.text.toString()
         if(noteText.isNotEmpty()) {
             viewModel.insertNote(Note(noteText))
+            val et: EditText = findViewById(R.id.input)
+            et.setText("")
             Toast.makeText(this, "$noteText Inserted", Toast.LENGTH_LONG).show()
         }
+    }
+
+    fun onClickClear(view: android.view.View) {
+        val et: EditText = findViewById(R.id.input)
+        et.setText("")
     }
 }
